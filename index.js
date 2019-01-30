0305ca7f19d1282bf65a8ca2c3c8c6d775791174
Vivic2106 <33154756+Victor2106@users.noreply.github.com>
2019-01-30 16:31:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+
+client.login("NTM2MjgwNjk3NDMwNzM2ODk3.Dy4Sfw.m3JT9bL80eYS2RZR5Zvp37ppDzs");
+
+client.commands = new Discord.Collection();
+
+fs.readdir("./Commandes/", (error, f) => {
+    if(error) console.log(error);
+
+    let commandes = f.filter(f => f.split(".").pop() === "js");
+    if(commandes.length <= 0) return console.log("Aucune commande trouvée !");
+
+    commandes.forEach((f) => {
+        let commande = require(`./Commandes/${f}`);
+        console.log(`${f} commande chargée !`);
+
+        client.commands.set(commande.help.name, commande);
+    });
+});
+
+fs.readdir("./Events/", (error, f) => {
+    if(error) console.log(error);
+    console.log(`${f.length} events chargés`);
+
+    f.forEach((f) => {
+        const events = require(`./Events/${f}`);
+        const event = f.split(".")[0];
+
+        client.on(event, events.bind(null, client));
+    });
+});
