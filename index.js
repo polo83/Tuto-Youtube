ccd6a95589dd346ef9587eca607019e012df63fb
Vivic2106 <33154756+Victor2106@users.noreply.github.com>
2019-01-30 16:33:15 +0100

    Fichier principale

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 
-client.login("votre token");
+client.login("token");
 
 client.commands = new Discord.Collection();
 
