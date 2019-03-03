56d927d4014f97ae22c9148732f4621245f61476
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-03 12:29:01 +0100

    Syntaxe fixed

--- Events/message.js
+++ Events/message.js
@@ -1,20 +1,16 @@
 const Discord = require('discord.js');
-const prefix = "!";
+const prefix = '!';
 
 module.exports = async(client, message) => {
 
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
+    if (message.author.bot || message.channel.type === 'dm') { return; }
+    if (!message.channel.permissionsFor(client.user).has('SEND_MESSAGES')) { return; }
+    if (!message.content.startsWith(prefix)) { return; }
 
-    if(!message.content.startsWith(prefix)) return;
+        let args = message.content.slice(prefix.length).trim().split(/ +/g);
+        let commande = args.shift();
+        let cmd = client.commands.get(commande);
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const commande = args.shift();
-
-    const cmd = client.commands.get(commande);
-
-    if(!cmd) return;
-
-    cmd.run(client, message, args);
-
+        if (!cmd) { return; }
+            cmd.run(client, message, args);
 };
