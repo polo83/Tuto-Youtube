680966c38dc8078c4cc2b75277a93576611f5995
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-03 12:32:34 +0100

    Syntaxe fixed

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
-
-client.login("token");
-
 client.commands = new Discord.Collection();
 
-fs.readdir("./Commandes/", (error, f) => {
-    if(error) console.log(error);
+const fs = require('fs');
 
-    let commandes = f.filter(f => f.split(".").pop() === "js");
-    if(commandes.length <= 0) return console.log("Aucune commande trouvée !");
 
-    commandes.forEach((f) => {
-        let commande = require(`./Commandes/${f}`);
-        console.log(`${f} commande chargée !`);
+fs.readdir('./Commandes/', (error, f) => {
+    if (error) { return console.error(error); }
+        let commandes = f.filter(f => f.split('.').pop() === 'js');
+        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
 
-        client.commands.set(commande.help.name, commande);
-    });
+        commandes.forEach((f) => {
+            let commande = require(`./Commandes/${f}`);
+            console.log(`${f} commande chargée !`);
+            client.commands.set(commande.help.name, commande);
+        });
 });
 
-fs.readdir("./Events/", (error, f) => {
-    if(error) console.log(error);
-    console.log(`${f.length} events chargés`);
+fs.readdir('./Events/', (error, f) => {
+    if (error) { return console.error(error); }
+        console.log(`${f.length} events chargés`);
 
-    f.forEach((f) => {
-        const events = require(`./Events/${f}`);
-        const event = f.split(".")[0];
-
-        client.on(event, events.bind(null, client));
-    });
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split('.')[0];
+            client.on(event, events.bind(null, client));
+        });
 });
+
+client.login("token");
