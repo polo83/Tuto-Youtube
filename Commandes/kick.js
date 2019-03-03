7be63b77e6ff5b6a5a014cd20900b174727b9df4
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-03 12:23:09 +0100

    Syntaxe fixed

--- Commandes/kick.js
+++ Commandes/kick.js
@@ -1,28 +1,35 @@
 const Discord = require('discord.js');
 
-module.exports.run = async(client, message, args) => {
+module.exports.run = (client, message, args) => {
+    if (!message.guild.member(message.author).hasPermission('KICK_MEMBERS')) { return message.channel.send('Vous n\'avez pas la permission !'); }
+    if (!message.guild.member(client.user).hasPermission('KICK_MEMBERS')) { return message.channel.send('Le bot n\'a pas la permission !'); }
+    if (message.mentions.users.size === 0) { return message.channel.send('Vous devez mentionner un utilisateur !'); }
 
-    if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send(`Vous n'avez pas la permission !`);
-
-    if(message.mentions.users.size === 0) {
-        return message.channel.send(`Vous devez mentionner un utilisateur !`);
-    }
-
-    let kick = message.guild.member(message.mentions.users.first());
-
-    if(!kick) {
-        return message.channel.send(`Je n'ai pas trouvé l'utilisateur !`);
-    }
-
-    if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.channel.send(`Le bot n'a pas la permission !`);
-
-    kick.kick().then(member => {
-
-        message.channel.send(`${member.user.username} est kick ! Par ${message.author.username}`);
-        message.mentions.users.first().send(`Vous êtes expulsé du serveur **${message.guild.name}** par ${message.author.username}`).catch(console.error);
-    });
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if (!kickMember) { return message.channel.send('Je n\'ai pas trouvé l\'utilisateur !'); }
+    
+        message.mentions.users.first().send(`Vous êtes kick du serveur **${message.guild.name}** par ${message.author.username}`)
+            .then((member) => {
+                kickMember.ban()
+                    .then((member) => {
+                        message.channel.send(`${member.user.username} est kick ! Par ${message.author.username}`);
+                    })
+                        .catch((err) => {
+                            if (err) { return console.error(err); }
+                        });
+            })
+                .catch((err) => {
+                    if (err) { console.error(err); }
+                        kickMember.ban()
+                            .then((member) => {
+                                message.channel.send(`${member.user.username} est kick ! Par ${message.author.username}`);
+                            })
+                                .catch((err) => {
+                                    if (err) { return console.error(err); }
+                                });
+                });
 };
 
 module.exports.help = {
-    name: "kick"
+    name: 'kick'
 };
