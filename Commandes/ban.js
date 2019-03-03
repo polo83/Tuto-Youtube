74e2eca89b58385a63bcd766688f22ba3ba8f935
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-03 12:25:54 +0100

    Syntaxe fixed

--- Commandes/ban.js
+++ Commandes/ban.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-module.exports.run = async(client, message, args) => {
+module.exports.run = (client, message, args) => {
     if (!message.guild.member(message.author).hasPermission('BAN_MEMBERS')) { return message.channel.send('Vous n\'avez pas la permission !'); }
     if (!message.guild.member(client.user).hasPermission('BAN_MEMBERS')) { return message.channel.send('Le bot n\'a pas la permission !'); }
     if (message.mentions.users.size === 0) { return message.channel.send('Vous devez mentionner un utilisateur !'); }
