f36943e49817fad1fc657b9fb410ce33ceef1da8
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-03 12:20:48 +0100

    Syntaxe fixed

--- Commandes/ban.js
+++ Commandes/ban.js
@@ -1,28 +1,35 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, message, args) => {
+    if (!message.guild.member(message.author).hasPermission('BAN_MEMBERS')) { return message.channel.send('Vous n\'avez pas la permission !'); }
+    if (!message.guild.member(client.user).hasPermission('BAN_MEMBERS')) { return message.channel.send('Le bot n\'a pas la permission !'); }
+    if (message.mentions.users.size === 0) { return message.channel.send('Vous devez mentionner un utilisateur !'); }
 
-    if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.channel.send(`Vous n'avez pas la permission !`);
-
-    if(message.mentions.users.size === 0) {
-        return message.channel.send(`Vous devez mentionner un utilisateur !`);
-    }
-
-    let ban = message.guild.member(message.mentions.users.first());
-
-    if(!ban) {
-        return message.channel.send(`Je n'ai pas trouvé l'utilisateur !`);
-    }
-
-    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.channel.send(`Le bot n'a pas la permission !`);
-
-    ban.ban().then(member => {
-
-        message.channel.send(`${member.user.username} est ban ! Par ${message.author.username}`);
-        message.mentions.users.first().send(`Vous êtes banni du serveur **${message.guild.name}** par ${message.author.username}`).catch(console.error);
-    });
+        let banMember = message.guild.member(message.mentions.users.first());
+        if (!banMember) { return message.channel.send('Je n\'ai pas trouvé l'utilisateur !'); }
+    
+        message.mentions.users.first().send(`Vous êtes banni du serveur **${message.guild.name}** par ${message.author.username}`)
+            .then((member) => {
+                banMember.ban()
+                    .then((member) => {
+                        message.channel.send(`${member.user.username} est ban ! Par ${message.author.username}`);
+                    })
+                        .catch((err) => {
+                            if (err) { return console.error(err); }
+                        });
+            })
+                .catch((err) => {
+                    if (err) { console.error(err); }
+                        banMember.ban()
+                            .then((member) => {
+                                message.channel.send(`${member.user.username} est ban ! Par ${message.author.username}`);
+                            })
+                                .catch((err) => {
+                                    if (err) { return console.error(err); }
+                                });
+                });
 };
 
 module.exports.help = {
-    name: "ban"
+    name: 'ban'
 };
