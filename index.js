ae41034c5da1a68aa50fcfd8b663a6fb36fc5686
polo83 <52241374+polo83@users.noreply.github.com>
2019-07-20 16:41:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-const fs = require('fs');
 
-fs.readdir('./Commandes/', (error, f) => {
-    if (error) { return console.error(error); }
-        let commandes = f.filter(f => f.split('.').pop() === 'js');
-        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
-
-        commandes.forEach((f) => {
-            let commande = require(`./Commandes/${f}`);
-            console.log(`${f} commande chargée !`);
-            client.commands.set(commande.help.name, commande);
-        });
+client.on('ready', () => {
+  console.log('je suis jamais la');
 });
 
-fs.readdir('./Events/', (error, f) => {
-    if (error) { return console.error(error); }
-        console.log(`${f.length} events chargés`);
+client.on('message', message => {
+  if (!message.guild) return;
 
-        f.forEach((f) => {
-            let events = require(`./Events/${f}`);
-            let event = f.split('.')[0];
-            client.on(event, events.bind(null, client));
+  if (message.content.startsWith('!kick')) {
+
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.kick('Optional reason that will display in the audit logs').then(() => {
+          message.reply(`${user.tag} a ete kick`);
+        }).catch(err => {
+          message.reply('I was unable to kick the member');
+          console.error(err);
         });
+      } else {
+        message.reply('That user isn\'t in this guild!');
+      }
+    } else {
+      message.reply('mentionne la personne a kick');
+    }
+  }
 });
 
-client.login('token');
+client.login('NjAwNjYwMzgwODEzMDk5MDEx.XTMbdw.8HTTWxzoO-TceMX_q-_k2J4zp3I');
+
+client.on('message', message => {
+  if (message.content === 'yo') {
+    message.channel.send('salut comment va tu ?');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'salut') {
+    message.channel.send('salut comment va tu ?');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 're') {
+    message.channel.send('re');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'tu fait du gta 5 rp ?') {
+    message.channel.send('oui Polo 83 fait du gta 5 rp');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'que fait tu ?') {
+    message.channel.send('je patiente pour que on me programme');
+  }
+});
+
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  if (!channel) return;
+  channel.send(`Bienvenu sur le serveur SMICOU,${member}`);
+});
+
+client.on('message', message => {
+  if (message.content === '${member}') {
+    message.reply(message.author.avatarURL);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'meilleure jeux') {
+    message.channel.send('merci beaucoup :star:  :star:  :star:  :star:  :star: ');
+  }
+});
